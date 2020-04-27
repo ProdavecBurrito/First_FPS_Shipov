b797584bbd208c64da5d9ad74c816c3f9228873e
Михаил <shipov-mihail@mail.ru>
2020-04-27 23:32:59 +0300

    WIP: Finished HW, except "baking light"

---
+++ Assets/Scripts/MiniMapCont.cs
@@ -0,0 +1,39 @@
+﻿
+using UnityEngine;
+
+public class MiniMapCont : MonoBehaviour
+{
+    [SerializeField]Transform player;
+    Camera mapCam;
+    Vector3 newPos;
+    [SerializeField] Shader repShader;
+
+    private void Start()
+    {
+        mapCam = GetComponent<Camera>();
+        player = FindObjectOfType<SinglePlayer>().transform;
+        repShader = Shader.Find("Toon/Basic Outline");
+
+        if (repShader)
+        {
+            mapCam.SetReplacementShader(repShader, "RenderType"); 
+        }
+    }
+
+    private void OnDisable()
+    {
+        mapCam.ResetReplacementShader();
+    }
+
+    private void LateUpdate()
+    {
+        if(player)
+        {
+            newPos = player.position;
+            newPos.y = transform.position.y;
+            transform.position = newPos;
+
+            transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+        }
+    }
+}
